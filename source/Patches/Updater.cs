27a16c63cf3e8d7087d9cc9c902de1f7b6ac18f6
JoaKing08 <126957098+JoaKing08@users.noreply.github.com>
2024-04-22 09:23:53 +0200

    JK v1.0.3 Source Code

--- source/Patches/Updater.cs
+++ source/Patches/Updater.cs
@@ -219,7 +219,7 @@ namespace TownOfUs
                 string githubURI = "";
                 if (updateType == "TOU")
                 {
-                    githubURI = "https://api.github.com/repos/eDonnes124/Town-Of-Us-R/releases/latest";
+                    githubURI = "https://api.github.com/repos/JoaKing08/Town-Of-Us-JK/releases/latest";
                 }
                 else if (updateType == "Submerged")
                 {
