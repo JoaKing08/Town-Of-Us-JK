2548eeb20062d9ac3229b18704dab1ca167ddb06
JoaKing08 <126957098+JoaKing08@users.noreply.github.com>
2024-04-24 23:54:39 +0200

    JK v1.0.4 Source Code

--- source/TownOfUs.cs
+++ source/TownOfUs.cs
@@ -32,7 +32,7 @@ namespace TownOfUs
     {
         public const string Id = "com.slushiegoose.townofus";
         public const string VersionString = "5.0.4";
-        public const string ModesVersionString = "1.0.3";
+        public const string ModesVersionString = "1.0.4";
         public static System.Version Version = System.Version.Parse(ModesVersionString);
         public const string VersionTag = "<color=#ff33fc></color>";
 
