952587e858a24b014f0d9a1aa8d94df04418dde0
JoaKing08 <126957098+JoaKing08@users.noreply.github.com>
2024-05-02 17:51:12 +0200

    JK v1.2.0 Source Code

--- source/Patches/LowLights.cs
+++ source/Patches/LowLights.cs
@@ -39,7 +39,8 @@ namespace TownOfUs
                 (player._object.Is(RoleEnum.Jester) && CustomGameOptions.JesterImpVision) ||
                 (player._object.Is(RoleEnum.Arsonist) && CustomGameOptions.ArsoImpVision) ||
                 (player._object.Is(RoleEnum.Vampire) && CustomGameOptions.VampImpVision) ||
-                player._object.Is(RoleEnum.SerialKiller))
+                player._object.Is(RoleEnum.SerialKiller) || player._object.Is(RoleEnum.JKNecromancer) ||
+                player._object.Is(RoleEnum.Jackal))
             {
                 __result = __instance.MaxLightRadius * GameOptionsManager.Instance.currentNormalGameOptions.ImpostorLightMod;
                 return false;
